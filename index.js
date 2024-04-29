25d1789671df4bdabf47a1c7d65c3912abbe4788
Taha <muntahahussainsample@gmail.com>
2024-04-29 15:19:05 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.blue("\n \t Wellcome to simple Calculator\n"));
+//Asking question from user through inquirer.
+let answer = await inquirer.prompt([
+    { message: "Enter the first Number", type: "number", name: "firstNumber" },
+    { message: "Enter the second Number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operator to perform action",
+        type: "list",
+        name: "operators",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"],
+    },
+]);
+//Using if statement
+if (answer.operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operators === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select correct Options");
+}
